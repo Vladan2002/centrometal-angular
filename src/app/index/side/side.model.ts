a24ef412b74bd71d1e7d954c05ac258e9c3784b9
vlado <vladancmiljanic123@gmail.com>
2025-05-08 09:43:16 +0200

    Added side component

---
+++ src/app/index/side/side.model.ts
@@ -0,0 +1,10 @@
+import {SideComponent} from './side.component';
+import{NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [SideComponent],
+  exports: [SideComponent]
+})
+export class SideModel {}
