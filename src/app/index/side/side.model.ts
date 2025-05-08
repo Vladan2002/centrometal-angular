7e0ff0bb2fa8f3e64f1414c8513ed83b372bef2e
vlado <vladancmiljanic123@gmail.com>
2025-05-08 10:50:00 +0200

    Side-bar initial commit

--- src/app/index/side/side.model.ts
+++ src/app/index/side/side.model.ts
@@ -1,8 +1,10 @@
 import {SideComponent} from './side.component';
 import{NgModule} from '@angular/core';
+import {SideBarModule} from './side-bar/side-bar.modul';
 
 @NgModule({
   imports: [
+    SideBarModule
   ],
   declarations: [SideComponent],
   exports: [SideComponent]
