6633c80024727293f32842a04f3f5528b9823701
vlado <vladancmiljanic123@gmail.com>
2025-05-09 09:24:06 +0200

    test

--- src/app/index/components/side/accordion/accordion.modul.ts
+++ src/app/index/components/side/accordion/accordion.modul.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccordionComponent } from './accordion.component';
-import {AccordionService} from '../../../../services/accordion.service';
+import {AccordionService} from '../../../../../services/accordion.service';
 @NgModule({
   declarations: [AccordionComponent],
   imports: [CommonModule],
--- src/app/index/components/side/side.model.ts
+++ src/app/index/components/side/side.model.ts
@@ -1,8 +1,12 @@
 import {SideComponent} from './side.component';
 import{NgModule} from '@angular/core';
+import {AccordionModule} from './accordion/accordion.modul';
+import {SideBarModule} from './side-bar/side-bar.modul';
 
 @NgModule({
   imports: [
+    AccordionModule,
+    SideBarModule
   ],
   declarations: [SideComponent],
   exports: [SideComponent]
