adfd9ab6e157db69a85e056e54b52bd1eb54a746
vlado <vladancmiljanic123@gmail.com>
2025-05-15 12:20:33 +0200

    .

--- src/app/index/components/main-content/main-content.modul.ts
+++ src/app/index/components/main-content/main-content.modul.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import {MainContentComponent} from './main-content.component';
 import {MainSliderModule} from './main-slider/main-slider.module';
+import {ProductsComponent} from "./products/products.component";
 
 @NgModule({
   declarations: [MainContentComponent],
-  imports: [
-    MainSliderModule
-  ],
+    imports: [
+        MainSliderModule,
+        ProductsComponent
+    ],
   exports: [MainContentComponent]
 })
 export class MainContentModule {}
