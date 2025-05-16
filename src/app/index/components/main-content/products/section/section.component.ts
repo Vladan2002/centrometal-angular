ef24e05773b0cc4f6e99bab99b6745e5b6ca0812
vlado <vladancmiljanic123@gmail.com>
2025-05-16 12:00:35 +0200

    New version for products

---
+++ src/app/index/components/main-content/products/section/section.component.ts
@@ -0,0 +1,40 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Section} from '../interfaces/products.interface';
+import {SectionsService} from '../../../../../../services/section.service';
+
+@Component({
+  selector: 'app-section',
+  standalone: false,
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.scss'
+})
+export class SectionComponent implements OnInit {
+  @Input() section!: Section;
+
+  constructor(private sectionService:SectionsService) {}
+  public limit:any;
+  loader:boolean = false;
+
+  ngOnInit(): void {
+   this.loadCards()
+  }
+
+  loadCards(): void {
+    this.limit = Array.from({ length: this.section.limit }, (_, i) => i);
+    this.loader = true;
+    setTimeout(() => this.fetchCards(), 1500);
+  }
+
+  fetchCards(): void {
+    this.sectionService.populateSectionCards(this.section).subscribe(
+      (res) => {
+        this.section = res;
+      },
+      (err) => {
+        console.error('Error', err);
+        this.loader = false;
+      }
+    );
+  }
+
+}
