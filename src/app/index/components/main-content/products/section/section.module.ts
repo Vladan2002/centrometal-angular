fb22b02f574b996db5cf024820f39aef4466e1b2
vlado <vladancmiljanic123@gmail.com>
2025-05-16 11:56:53 +0200

    .

--- src/app/index/components/main-content/products/section/section.module.ts
+++ src/app/index/components/main-content/products/section/section.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SectionComponent} from './section.component';
-import {SkeletonCardModule} from '../../../../../skeleton-card/skeleton-card.module';
+import {SkeletonCardModule} from '../skeleton-card/skeleton-card.module';
 import {CardModule} from './card/card.module';
 
 @NgModule({
