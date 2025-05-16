a34a7c37be8b7abfd8e85d102c67f04ce13abb79
vlado <vladancmiljanic123@gmail.com>
2025-05-16 12:08:26 +0200

    New version for products

--- src/app/index/components/main-content/products/products.module.ts
+++ src/app/index/components/main-content/products/products.module.ts
@@ -2,12 +2,11 @@ import {NgModule} from '@angular/core';
 import {ProductsComponent} from './products.component';
 import {CommonModule} from '@angular/common';
 import {SectionsService} from '../../../../../services/section.service';
-import {CardModule} from './section/card/card.module';
 import {SectionModule} from './section/section.module';
 
 @NgModule({
   declarations: [ProductsComponent],
-    imports: [CommonModule, CardModule, SectionModule],
+    imports: [CommonModule,SectionModule],
   providers: [SectionsService],
   exports: [ProductsComponent]
 })
