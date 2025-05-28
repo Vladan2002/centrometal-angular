2070ffa250ab10ff855c16649e95f75f276678bf
vlado <vladancmiljanic123@gmail.com>
2025-05-28 15:55:36 +0200

    Similar products

--- src/app/open-product/open-product.module.ts
+++ src/app/open-product/open-product.module.ts
@@ -3,10 +3,13 @@ import {OpenProductComponent} from './open-product.component';
 import {NgModule} from '@angular/core';
 import {ProductSliderSkeletonModule} from './components/loaders/product-slider-skeleton/product-slider-skeleton.module';
 import {ProductDescriptionSkeletonModule} from './components/loaders/product-description-skeleton/product-description-skeleton.module';
+import {CardModule} from './components/card/card.module';
+import {SkeletonCardModule} from './components/loaders/skeleton-card/skeleton-card.module';
+import {NoProductsModule} from './components/no-products/no-products.module';
 
 @NgModule({
   declarations: [ OpenProductComponent ],
-  imports: [CommonModule, ProductSliderSkeletonModule, ProductDescriptionSkeletonModule],
+  imports: [CommonModule, ProductSliderSkeletonModule, ProductDescriptionSkeletonModule, CardModule, SkeletonCardModule, NoProductsModule],
   providers:[],
   exports: [ OpenProductComponent ]
 })
