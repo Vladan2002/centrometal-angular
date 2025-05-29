014e069fa769cb62e7cdcd765160dec83dd3d072
vlado <vladancmiljanic123@gmail.com>
2025-05-29 11:20:11 +0200

    initial commit

---
+++ src/app/open-product/components/product-side/newsletter/newsletter.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {NewsletterComponent} from './newsletter.component';
+
+@NgModule({
+  declarations: [NewsletterComponent],
+  imports: [],
+  exports: [NewsletterComponent]
+})
+export class NewsletterModule { }
