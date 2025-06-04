5866c4f7940ff2fddad05e315ce6f7d9b705b267
vlado <vladancmiljanic123@gmail.com>
2025-06-04 09:49:42 +0200

    initial commit

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -2,11 +2,15 @@ import { IndexComponent } from './index.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideModule } from './components/side/side.module';
-import {MainContentModule} from './components/main-content/main-content.modul';  // ispravan import
+import {MainContentModule} from './components/main-content/main-content.modul';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [CommonModule, SideModule, MainContentModule],
+  imports: [CommonModule, SideModule, MainContentModule,
+  RouterModule.forChild([
+    {path:'', component: IndexComponent},
+  ])],
   exports: [IndexComponent]
 })
 export class IndexModule { }
