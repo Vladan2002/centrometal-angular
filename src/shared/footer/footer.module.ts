9f0671250dbf04a541e5aced0e01da1e7959f1c1
vlado <vladancmiljanic123@gmail.com>
2025-06-04 09:50:46 +0200

    .

--- src/shared/footer/footer.module.ts
+++ src/shared/footer/footer.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {FooterComponent} from './footer.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [],
+  imports: [CommonModule],
   providers: [],
   exports: [FooterComponent]
 })
